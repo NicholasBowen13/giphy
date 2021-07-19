8038b1e96935487628359dcc45d63ec195e42df8
Nick <Nickbow1388@gmail.com>
2021-07-19 18:27:07 +0000

    Update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getImageUrlFrom
 import os
 
 # -- Initialization section --
@@ -14,3 +14,12 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+@app.route('/yourgif', methods = ["GET", "POST"])
+def yourgif():
+    user_response = request.form["gifchoice"]
+    message =getImageUrlFrom(user_response)
+    return render_template("yourgif.html", time=datetime.now(), message = message)
+    #add datetime.now() browser to trick our browser into updating the css if we make any changes
+
+    
--- model.py
+++ model.py
@@ -1,2 +1,6 @@
+import requests
+
 def getImageUrlFrom(query):
-    pass
+    giphy_request_link = "https://api.giphy.com/v1/gifs/search?api_key=N3klsgYWD5rIOHzrRQePi7iMqrEBYEKD&q=" + query + " cat&limit=25&offset=0&rating=g&lang=en" 
+    response = requests.get(giphy_request_link).json()
+    return response ["data"][0]["images"]["fixed_height"]["url"]
